dc17e50d29fdfaf5ae51073ae015fbe8a0e3d414
Arash Javanmard <a.javan.gh@gmail.com>
2016-01-16 16:19:07 +0100

    adding navigation-drawer

--- app/src/main/java/de/vitalife/vitalife/category/CategoryActivity.java
+++ app/src/main/java/de/vitalife/vitalife/category/CategoryActivity.java
@@ -2,6 +2,8 @@ package de.vitalife.vitalife.category;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 
 import de.vitalife.vitalife.BaseActivity;
@@ -23,12 +25,19 @@ public class CategoryActivity extends BaseActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this,
+                drawer, toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+
+        drawer.setDrawerListener(toogle);
+        toogle.syncState();
+
         FragmentManager fm = getSupportFragmentManager();
-        CategoryFragment fragment = (CategoryFragment) fm.findFragmentById(R.id.container);
+        CategoryFragment fragment = (CategoryFragment) fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
             fragment = new CategoryFragment();
-            fm.beginTransaction().add(R.id.container, fragment)
+            fm.beginTransaction().add(R.id.fragment_container, fragment)
                     .addToBackStack(fragment.TAG).commit();
         }
     }
